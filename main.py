50d3ad4567b3c214e277442cae8ae70312666f42
Samarth Sharma <98225415+SamarthSharma12@users.noreply.github.com>
2022-11-17 05:36:40 +0530

    Update main.py
    Add google search ,GPT3 ,youtube search and weather search feature

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import pyautogui
 import webbrowser as wb
 import wikipedia
 import openai 
-import time
+from time import sleep
+import pywhatkit # for youtube video
 
 import random
 
@@ -33,14 +34,6 @@ def change_voice(voice):
     
 # This function will give you tell you about the time 
 def time(Region,City):
-    if(Region is None ):
-      response = requests.get('https://timeapi.io/api/Time/current/zone?timeZone={0}/{1}'.format("Asia","Calcutta"))
-      python_data= json.loads(response.text)
-      time = python_data.get('time')
-      date = python_data.get('dateTime')
-      audio = "The date of {0} is{1} and time is {2}".format(City,date,time)
-      give_audio(audio)
-    else:
       response = requests.get('https://timeapi.io/api/Time/current/zone?timeZone={0}/{1}'.format(Region,City))
       python_data= json.loads(response.text)
       time = python_data.get('time')
@@ -60,32 +53,32 @@ def greeting():
   else:
     give_audio("Good Night Sir")
     
-    
+def greet():
+  give_audio("I hope you are having a great time")
 
-def gpt3(query):
-  openai.api_key = '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'
+def gpt3(s):
+  openai.api_key = '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'
   response = openai.Completion.create(
-    engine = "davinci-instruct-beta",
-    prompt = query,
-    temperature =0.1,
-    max_token = 1000,
-    top_p =1,
-    frequency_penalty =0,
-    presence_penalty =0
+    model="text-curie-001",
+    prompt=s,
+    temperature=0.2,
+    max_tokens=1000,
+    top_p=1,
+    frequency_penalty=1,
+    presence_penalty=0
   )
-  content = response.choices[0].text.split('.')
-  print(content)
-  return response.choices[0].text
-  
+  name = response['choices'][0].text
+  print(name)
+  give_audio(name)
   
 def wishme(name):
     give_audio("Welcome back {}".format(name))
     give_audio("How may I help you")
     return
     
-def take_cmd():
+"""def take_cmd():
   query = input("How may I help you?")
-  return query
+  return query"""
 
 
 def takeCommandMic():
@@ -105,62 +98,74 @@ def takeCommandMic():
   
 def sendwhatappmsg(ph_no,msg):
   Message = msg
-  wb.open('https://web.whatsapp.com/send?phone='+ph_no+'&text='+msg)
-  time.sleep(10)
+  wb.open('https://web.whatsapp.com/send?phone='+ph_no+'&text='+Message)
+  sleep(10)
   pyautogui.press('enter')
+  
+def searchgoogle():
+  give_audio("what should I search for?")
+  search = takeCommandMic()
+  wb.open('https://www.google.com/search?q={}'.format(search ))
+  
 
 
 if __name__ == "__main__":
-         if 'M' in takeCommandMic():
-            change_voice(1)
-            give_audio("What your name sir")
-            name = takeCommandMic()
-            wishme(name)
-         else:
-            change_voice(2)
-            give_audio("What your name Ma'am")
-            name = takeCommandMic()
-            wishme(name)
-         while True:
-            query = takeCommandMic().lower()
-            if 'time' in query :
-              give_audio("Say Region")
-              region = takeCommandMic()
-              give_audio("Say City")
-              City = takeCommandMic()
-              time(region,City)
-            if 'greet' in query:
-              greeting()
-              
-            if 'message' in query:
-              user_name = {"Rishabh":"+91 9354471042","Gautam":"+91 9311450881","Samarth":"+91 8979090473","Shobhana":"+91 9368134020","Sanjay":"+91 9412861605",
-                           "Seema Mishra":"+91 9452664323","Suket":"+91 8237862436","Vijaya":"+91 9121952003","Shubham":"+91 9582789002",
-                           "Aryaman":"+91 9799805800","Satvik":"+91 9110895517"}
-              try:
-                give_audio("To whom you want to send the Whats app message")
-                name = takeCommandMic()
-                ph_no = user_name[name]
-                give_audio("What is the message?")
-                message = takeCommandMic()
-                sendwhatappmsg(ph_no,message)
-                give_audio('message to send the message')
-              except Exception as e:
-                print(e)
-                give_audio("unable to send the message")
-                
-            if 'wikipedia' in query:
-              give_audio("searching on wikipeida...")
-              query = query.replace("wikipedia","")
-              result = wikipedia.summary(query,sentences = 2)
-              print(result)
-              give_audio(result)
-              
-            if 'stop' in query:
-              break
-            else:
-              query = takeCommandMic()
-              gpt3(query)
-            
+    change_voice(1)
+    """give_audio("What your name Sir")
+    name = takeCommandMic()"""
+    wishme("Samarth")
+    greet()
+    while True:
+      query = takeCommandMic().lower()
+      if 'current' and 'time' in query:
+        time('Asia','Calcutta')
+      elif 'time' in query :
+        give_audio("Say Region")
+        region = takeCommandMic()
+        give_audio("Say City")
+        City = takeCommandMic()
+        time(region,City)
+      elif 'greet' in query:
+        greeting()
+        
+      elif 'message' in query:
+        user_name = {"Samarth":"+91 8979090473","Shobhana":"+91 9368134020","Sanjay":"+91 9412861605",
+                      }
+        try:
+          give_audio("To whom you want to send the Whats app message")
+          name = takeCommandMic()
+          ph_no = user_name[name]
+          give_audio("What is the message?")
+          message = takeCommandMic()
+          sendwhatappmsg(ph_no,message)
+          give_audio("Message has been send")
+        except Exception as e:
+          print(e)
+          give_audio("unable to send the message")
+          
+      elif 'wikipedia' in query:
+        give_audio("searching on wikipeida...")
+        query = query.replace("wikipedia","")
+        result = wikipedia.summary(query,sentences = 2)
+        print(result)
+        give_audio(result)
+      elif 'google' in query:
+        searchgoogle()
+        
+      elif 'youtube' in query:
+        give_audio("what should I search for on youtube?")
+        topic = takeCommandMic()
+        pywhatkit.playonyt(topic)
+      elif 'weather' in query:
+        city = query.replace("weather","")
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=dc1bd9d9718ec49bb299a339416ac853'.format(city)
+        
+  
+      else:
+        gpt3(query)
+        break
+        
+# http://api.openweathermap.org/data/2.5/weather?q=Agra&units=imperial&appid=dc1bd9d9718ec49bb299a339416ac853
       
       
 
